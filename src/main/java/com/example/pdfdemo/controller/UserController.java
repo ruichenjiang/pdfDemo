95322796ceabac7bdcbe9840d461a9e35d0910f2
jun <383081437@qq.com>
2022-12-04 21:59:07 +0800

    dev-jun:增加getUser方法

--- src/main/java/com/example/pdfdemo/controller/UserController.java
+++ src/main/java/com/example/pdfdemo/controller/UserController.java
@@ -1,16 +1,26 @@
 package com.example.pdfdemo.controller;
 
+import com.example.pdfdemo.entity.User;
 import com.example.pdfdemo.service.UserService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Service
+import javax.annotation.Resource;
+
+@RestController
 public class UserController {
-
+    @Resource
     private UserService userService;
 
     @PostMapping("/test")
     public String test(){
         return "ok";
     }
+
+    @PostMapping("/getUser")
+    public User getUser(){
+        return userService.getUser();
+    }
+
 }
