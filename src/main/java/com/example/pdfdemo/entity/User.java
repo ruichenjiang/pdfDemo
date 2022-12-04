a906246bf1c5f036fef8969809ab33a2e7eb24e9
jun <383081437@qq.com>
2022-12-04 21:26:17 +0800

    dev-jun:用户模块

---
+++ src/main/java/com/example/pdfdemo/entity/User.java
@@ -0,0 +1,15 @@
+package com.example.pdfdemo.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String id;
+    private String userId;
+    private String userName;
+    private String dispayName;
+    private String address;
+    private int gender;
+
+
+}
